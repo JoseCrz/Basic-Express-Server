aed28d836607b251e29a617b2850b45b9ec61c2d
Jose Cuevas <cueramjose@gmail.com>
2018-10-31 22:30:51 -0600

    Added some middleware examples on App.js

--- app.js
+++ app.js
@@ -4,6 +4,31 @@ const express = require('express')
 
 const app = express()
 
+app.use( (request, response, next) => {
+    console.log(`In the middleware!`)
+    next()
+})
+
+app.use( (request, response, next) => {
+    console.log(`In the second middleware!`)
+    next()
+})
+
+app.use( (request, response, next) => {
+    console.log(`In the third middleware!`)
+    next()
+})
+
+app.use( (request, response, next) => {
+    console.log(`In the fourth middleware`)
+    next()
+})
+
+app.use( (request, response, next) => {
+    console.log(`In the final middleware`)
+    response.send(`<h1>Hello from Express.js!</h1>`)
+}) 
+
 const server = http.createServer(app)
 
 server.listen(3000)

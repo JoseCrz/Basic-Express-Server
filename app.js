19be9f2a9d8ea3f8dbdee0185ceb32a563c2ee87
Jose Cuevas <cueramjose@gmail.com>
2018-11-01 10:41:50 -0600

    Installed Body-Parser package & added basic functionality to get data sent by the user

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.use('/add-product', (request, response, next) => {
     console.log(`add product middleware`)
-    response.send(`<h1>"Add Product" page`)
+    response.send(`<form action="/product" method="POST"> <input type="text" name="title" /> <button type="subtmit">Add product!</button> </form>`)
+})
+
+app.use('/product', (request, response, next) => {
+    console.log(request.body)
+    response.redirect('/')
 })
 
 app.use('/', (request, response, next) => {

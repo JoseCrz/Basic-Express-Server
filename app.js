21f087b4eb95fc03b89ab49ab2b33b3bef7fda2e
Jose Cuevas <cueramjose@gmail.com>
2018-11-01 11:27:22 -0600

    Added Express Router & created routes files

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product', (request, response, next) => {
-    console.log(`add product middleware`)
-    response.send(`<form action="/product" method="POST"> <input type="text" name="title" /> <button type="subtmit">Add product!</button> </form>`)
-})
+app.use(adminRoutes)
+app.use(shopRoutes)
 
-app.use('/product', (request, response, next) => {
-    console.log(request.body)
-    response.redirect('/')
-})
-
-app.use('/', (request, response, next) => {
-    console.log(`Main page URL`)
-    response.send(`<h1>Main Page</h1>`)
-})
 
 app.listen(3000)
---
+++ routes/shop.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (request, response, next) => {
+    console.log(`Main page URL`)
+    response.send(`<h1>Main Page</h1>`)
+})
+
+module.exports = router

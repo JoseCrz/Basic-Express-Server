623d5b268602008b90dc32fe9c196646a94d5d0e
Jose Cuevas <cueramjose@gmail.com>
2018-11-04 18:43:08 -0600

    Added saving to file functionality to product Model, there's an error while trying to display the products

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,5 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +7,29 @@ module.exports = class Product {
     }
 
     save () {
+        const filePath = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+        fs.readFile(filePath, (error, fileContent) => {
+            let products = []
+            if (!error) {
+                products = JSON.parse(fileContent)
+            }
+            products.push(this)
+
+            fs.writeFile(filePath, JSON.stringify(products), (error) => {
+                console.log(error)
+            })
+            
+        })
         products.push(this)
     }
 
     static fetchAll () {
-        return products
+        const filePath = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+        fs.readFile(filePath, (error, fileContent) => {
+            if (error) {
+                return []
+            }
+            return JSON.parse(fileContent)
+        })
     }
 }

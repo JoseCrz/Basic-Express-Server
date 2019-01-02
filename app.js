510c564d85868a88669d38fe2acd7dddf6559e60
Jose Cuevas <cueramjose@gmail.com>
2019-01-02 11:02:04 -0600

    Started to redefine the Product Model to work with MongoDB

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
-// const adminRoutes = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
 // const shopRoutes = require('./routes/shop')
 const errorsController = require('./controllers/errorsController')
 
@@ -22,7 +22,7 @@ app.use((request, response, next) => {
     
 })
 
-// app.use('/admin',adminRoutes)
+app.use('/admin',adminRoutes)
 // app.use(shopRoutes)
 
 app.use(errorsController.get404)
